c2764ee254dee0b4b02cc1578d8463c16e376b73
Kevin Lam <lam.kevin86@gmail.com>
2018-02-22 11:40:12 -0800

    Adds modal functionality

--- client/components/gallery.jsx
+++ client/components/gallery.jsx
@@ -23,21 +23,39 @@ export default class Gallery extends React.Component {
         <h1>Photo Gallery</h1>
         <p>There are <strong>{this.state.images.length}</strong> photos in this gallery</p>
 
-          <div className="selected">
-            {console.log(this.state.images)}
-            {this.state.images.map((image, index) => {
-              return <div key={index}><Photo image={image} /></div>
-            })
-            }
-          </div>          
+          <img src={this.state.images[0]} data-toggle="modal" data-target="#exampleModal" />
 
-          <div className="slider-nav">
-            {console.log(this.state.images)}
-            {this.state.images.map((image, index) => {
-              return <div key={index}><Photo image={image} /></div>
-            })
-            }
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModal" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-body">
+
+                    <div className="selected">
+                      {console.log(this.state.images)}
+                      {this.state.images.map((image, index) => {
+                        return <div key={index}>
+                                <Photo image={image} />
+                                <p>Image <strong>{index + 1}</strong> out of {this.state.images.length}</p>
+                        </div>
+                      })
+                      }
+                    </div>          
+
+                    <div className="slider-nav">
+                      {console.log(this.state.images)}
+                      {this.state.images.map((image, index) => {
+                        return <div key={index}><Photo image={image} /></div>
+                      })
+                      }
+                    </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
           </div>
+
       </div>
     )
   }
